00843e1ed141e669a42818086e7e1c9682460911
matveev96 <sasha.matveev1996@icloud.com>
2024-12-16 22:51:42 +0300

    work with Controlled input, FLUX

--- src/stories/input.stories.tsx
+++ src/stories/input.stories.tsx
@@ -1,4 +1,5 @@
 import React, {ChangeEvent, useRef, useState} from 'react';
+import {action} from "@storybook/addon-actions";
 
 export default {
     title: "input"
@@ -32,6 +33,37 @@ export const GetValueOfUncontrolledInputBybuttonPress = () => {
 
     </>
 };
+
+export const ControlledInput = () => {
+    const [parentValue, setParentValue] = useState("")
+    const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        setParentValue(e.currentTarget.value)
+    }
+    return <input value={parentValue} onChange={onChangeHandler}/>
+}
+
+export const ControlledCheckbox = () => {
+    const [parentValue, setParentValue] = useState(true)
+    const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        setParentValue(e.currentTarget.checked)
+    }
+    return <input type="checkbox" checked={parentValue} onChange={onChangeHandler}/>
+}
+
+export const ControlledSelect = () => {
+    const [parentValue, setParentValue] = useState<string | undefined>(undefined)
+    const onChangeHandler = (e: ChangeEvent<HTMLSelectElement>) => {
+        setParentValue(e.currentTarget.value)
+    }
+    return <select value={parentValue} onChange={onChangeHandler}>
+        <option>none</option>
+        <option value="1">Minsk</option>
+        <option value="2">Kiev</option>
+        <option value="3">Boston</option>
+        <option value="4">LA</option>
+
+    </select>
+}
 
 export const ControlledInputWithFixedValue = () => <input value={"qwerty"}/>;
 
